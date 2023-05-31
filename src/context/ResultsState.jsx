595c6a1836a3ae63675f942b2bd684b789037dba
test-bot-69 <testbot.1831@gmail.com>
2023-05-31 22:34:39 +0530

    console logged key

--- src/context/ResultsState.jsx
+++ src/context/ResultsState.jsx
@@ -19,6 +19,7 @@ const ResultsState = (props) => {
       setProgress(20)
 
       // fetch request
+      console.log('THINGSPEAK:', VITE_THINGSPEAK_API_KEY)
       const response = await fetch(`https://api.thingspeak.com/channels/2159620/fields/1.json?api_key=${VITE_THINGSPEAK_API_KEY}`)
 
       setProgress(40)
